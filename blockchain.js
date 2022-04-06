1835e69fb091226bb397f9b7d1514324daf75e3b
Dvir <44424737+dvirtayeb@users.noreply.github.com>
2022-04-06 12:45:16 +0300

    add mempool and stuff

--- blockchain.js
+++ blockchain.js
@@ -7,26 +7,128 @@ const bloomFilter = new PartitionedBloomFilter(10, 5, 0.001)
 
 class Blockchain {
 
-    constructor(genesisBlock) {
-        this.blocks = []
-        this.addBlock(genesisBlock)
+    constructor() {
+        this.chain = [this.createGenesisBlock()]
         this.difficulty = 2 // number of zero before target hash
         this.bfilter = bloomFilter
         this.memPool = []
+        this.miningReward = 100
+        this.burnAddress = "0x000000000000000000000000000000000000dead"
+
 
     }
+    createGenesisBlock(){
+        return new Block(Date.now(),"Genesis Block",'')
+    }
 
-    addBlock(block, miningRewardAdress) {
-        if(this.blocks.length == 0) {
-            block.previousHash = "0000000000000000"
-            block.hash = this.generateHash(block)
-        }
-        // block.mineBlock(this.difficulty)
-        // console.log("Block successfully mined.")
-        this.blocks.push(block)
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+     // addBlock(block, miningRewardAdress) {
+    //     if(this.blocks.length == 0) {
+    //         block.previousHash = "0000000000000000"
+    //         block.hash = this.generateHash(block)
+    //     }
+    //     // block.mineBlock(this.difficulty)
+    //     // console.log("Block successfully mined.")
+    //     this.blocks.push(block)
+    //     //this.bfilter.add(block.hash)
+    //     // let transaction = new Transaction(null, miningRewardAdress, this.miningReward)
+    //     // this.memPool.push(transaction)
+    // }
+
+    minePendingTransactions(miningRewardAddress){
+        createTransaction(Transaction(null,miningRewardAddress,this.miningReward))
+        
+        let block=new Block(Date.now(),this.pendingTransactions,this.getLatestBlock().hash)
+        block.mineBlock(this.difficulty)
+        console.log('Block successfully mines!')
+        this.chain.push(block)
         //this.bfilter.add(block.hash)
-        // let transaction = new Transaction(null, miningRewardAdress, this.miningReward)
-        // this.memPool.push(transaction)
+        this.memPool=[]
+    }
+
+    calculateFee(block){
+        let fee = block.index;
+        return fee
+    }
+    
+    addTransaction(transaction) {
+        if (!transaction.from || !transaction.to) {
+          throw new Error('Transaction must include from and to address');
+        }
+    
+        // Verify the transactiion
+        if (!transaction.isValid()) {
+          throw new Error('Cannot add invalid transaction to chain');
+        }
+        
+        if (transaction.amount <= 0) {
+          throw new Error('Transaction amount should be higher than 0');
+        }
+        
+        // Making sure that the amount sent is not greater than existing balance
+        const walletBalance = this.getBalanceOfAddress(transaction.from);
+        if (walletBalance < transaction.amount) {
+          throw new Error('Not enough balance');
+        }
+    
+        // Get all other pending transactions for the "from" wallet
+        const pendingTxForWallet = this.pendingTransactions
+          .filter(tx => tx.from === transaction.from);
+    
+        // If the wallet has more pending transactions, calculate the total amount
+        // of spend coins so far. If this exceeds the balance, we refuse to add this
+        // transaction.
+        if (pendingTxForWallet.length > 0) {
+          const totalPendingAmount = pendingTxForWallet
+            .map(tx => tx.amount)
+            .reduce((prev, curr) => prev + curr);
+    
+          const totalAmount = totalPendingAmount + transaction.amount;
+          if (totalAmount > walletBalance) {
+            throw new Error('Pending transactions for this wallet is higher than its balance.');
+          }
+        }
+                                        
+    
+        this.memPool.push(transaction);
+        debug('transaction added: %s', transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for (const block of this.chain) {
+        for (const trans of block.transactions) {
+            if (trans.from === address) {
+            balance -= trans.amount;
+            }
+
+            if (trans.to === address) {
+            balance += trans.amount;
+            }
+        }
+        }
+
+        debug('getBalanceOfAdrees: %s', balance);
+        return balance;
+    }
+
+    getAllTransactionsForWallet(address) {
+        const txs = [];
+    
+        for (const block of this.chain) {
+          for (const tx of block.transactions) {
+            if (tx.from === address || tx.to === address) {
+              txs.push(tx);
+            }
+          }
+        }
+    
+        debug('get transactions for wallet count: %s', txs.length);
+        return txs;
     }
 
     getNextBlock(transactions) {
@@ -56,12 +158,25 @@ class Blockchain {
         return this.blocks[this.blocks.length - 1]
     }
 
-    // hasChainValid(){
-        
-    // }
+    burnToken(fromAddress, amount){
+        this.addTransaction(new Transaction(fromAddress, this.burnAddress, amount))
+    }
+
+    isChainValid(){
+        for (let i = 1; i < this.chain.length; i++) {
+          const currentBlock=this.chain[i]
+          const previousBlock=this.chain[i-1]
+          if(!currentBlock.hasValidTransaction()){return false}
+          if(currentBlock.hash !== currentBlock.calculateHash()){
+           return false
+          }
+          
+          if(currentBlock.previousHash !== previousBlock.hash){
+            return false
+          }
+        }
+       return true
+     }
 }
 
 module.exports = Blockchain
-
-
-
